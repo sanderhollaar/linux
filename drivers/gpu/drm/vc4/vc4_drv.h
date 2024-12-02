ee8514f1eae451b4d7dd0ac25af31228a8bd2fe9
Dave Stevenson <dave.stevenson@raspberrypi.com>
2024-12-02 16:49:50 +0000

    Revert "drm/vc4: txp: Introduce structure to deal with revision differences"
    This reverts commit d124029a719515661e2de9bd9c0c91329b901ad0.

--- drivers/gpu/drm/vc4/vc4_drv.h
+++ drivers/gpu/drm/vc4/vc4_drv.h
@@ -547,11 +547,7 @@ struct vc4_crtc_data {
 	int hvs_output;
 };
 
-struct vc4_txp_data {
-	struct vc4_crtc_data	base;
-};
-
-extern const struct vc4_txp_data vc4_txp_data;
+extern const struct vc4_crtc_data vc4_txp_crtc_data;
 
 struct vc4_pv_data {
 	struct vc4_crtc_data	base;
